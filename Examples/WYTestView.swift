c8ec7b37eee772931166e78df57c7abd3edd0a19
jacke·xu <jacke.xu@jackexu.lan>
2020-12-11 18:05:01 +0800

    新增链式编程解决方案，新增设置渐变。边框。原角等方法

---
+++ Examples/WYTestView.swift
@@ -0,0 +1,21 @@
+//
+//  WYTestView.swift
+//  WYBasisKit
+//
+//  Created by jacke·xu on 2020/12/10.
+//  Copyright © 2020 jacke·xu. All rights reserved.
+//
+
+import UIKit
+
+class WYTestView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Examples/WYTestViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WYTestViewController.swift
+//  WYBasisKit
+//
+//  Created by jacke·xu on 2020/12/11.
+//  Copyright © 2020 jacke·xu. All rights reserved.
+//
+
+import UIKit
+
+class WYTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
