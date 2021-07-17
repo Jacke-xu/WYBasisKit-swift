1bbe91ef301b553bbcb2d0beca1fca1bfe5a78de
jacke·xu <mobileappdvlp@icloud.com>
2021-07-17 16:25:31 +0800

    调整多代理解决方案

--- WYBasisKit/Optional/ProtocolManager/WYProtocolManager.swift
+++ WYBasisKit/Optional/ProtocolManager/WYProtocolManager.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 
-@objc protocol WYMultiDelegate: NSObjectProtocol {
-    
+@objc protocol WYProtocoDelegate: NSObjectProtocol {
     @objc optional func test()
 }
 
@@ -19,36 +18,28 @@ struct WYProtocolManager {
     private let multiProtocol: NSHashTable<AnyObject> = NSHashTable.weakObjects()
     
     func test() {
-        
         invoke { $0.test!() }
     }
     
-    func add(_ delegate: WYMultiDelegate) {
-        
+    func add(delegate: WYProtocoDelegate) {
         multiProtocol.add(delegate)
     }
     
-    func remove(_ delegate: WYMultiDelegate) {
-        
+    func remove(delegate: WYProtocoDelegate) {
         invoke {
-          
             if $0 === delegate as AnyObject {
-                
                 multiProtocol.remove($0)
             }
         }
     }
     
     func removeAll() {
-        
         multiProtocol.removeAllObjects()
     }
     
-    private func invoke(_ invocation: (WYMultiDelegate) -> Void) {
-        
+    private func invoke(_ invocation: (WYProtocoDelegate) -> Void) {
       for delegate in multiProtocol.allObjects.reversed() {
-        
-        invocation(delegate as! WYMultiDelegate)
+        invocation(delegate as! WYProtocoDelegate)
       }
     }
 }
