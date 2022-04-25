fac0a2e9610fd677f262ef646e251416fe0117f7
Jacke·xu <mobileAppDvlp@icloud.com>
2022-04-25 17:01:51 +0800

    提交IJKMediaFramework.framework

--- Examples/Controller/WYTestLiveStreamingController.swift
+++ Examples/Controller/WYTestLiveStreamingController.swift
@@ -9,6 +9,162 @@
 import UIKit
 
 class WYTestLiveStreamingController: UIViewController {
+    
+    let player: WYLivePlayer = WYLivePlayer()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        let shouldAutoplay = UIButton(type: .custom)
+        shouldAutoplay.setTitle("shouldAutoplay", for: .normal)
+        shouldAutoplay.backgroundColor = .wy_random
+        shouldAutoplay.addTarget(self, action: #selector(shouldAutoplay(sender:)), for: .touchUpInside)
+        view.addSubview(shouldAutoplay)
+        shouldAutoplay.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.top.equalToSuperview().offset(wy_navViewHeight)
+        }
+        
+        let mute = UIButton(type: .custom)
+        mute.backgroundColor = .wy_random
+        mute.setTitle("mute", for: .normal)
+        mute.addTarget(self, action: #selector(mute(sender:)), for: .touchUpInside)
+        view.addSubview(mute)
+        mute.snp.makeConstraints { make in
+            make.left.equalTo(shouldAutoplay.snp.right)
+            make.top.equalTo(shouldAutoplay)
+        }
+        
+        let play = UIButton(type: .custom)
+        play.backgroundColor = .wy_random
+        play.setTitle("play", for: .normal)
+        play.addTarget(self, action: #selector(play(sender:)), for: .touchUpInside)
+        view.addSubview(play)
+        play.snp.makeConstraints { make in
+            make.left.equalTo(mute.snp.right)
+            make.top.equalTo(mute)
+        }
+        
+        let pause = UIButton(type: .custom)
+        pause.backgroundColor = .wy_random
+        pause.setTitle("pause", for: .normal)
+        pause.addTarget(self, action: #selector(pause(sender:)), for: .touchUpInside)
+        view.addSubview(pause)
+        pause.snp.makeConstraints { make in
+            make.left.equalTo(shouldAutoplay)
+            make.top.equalTo(mute.snp.bottom)
+        }
+        
+        let stop = UIButton(type: .custom)
+        stop.backgroundColor = .wy_random
+        stop.setTitle("stop", for: .normal)
+        stop.addTarget(self, action: #selector(stop(sender:)), for: .touchUpInside)
+        view.addSubview(stop)
+        stop.snp.makeConstraints { make in
+            make.left.equalTo(pause.snp.right)
+            make.top.equalTo(mute.snp.bottom)
+        }
+        
+        let url = UIButton(type: .custom)
+        url.backgroundColor = .wy_random
+        url.setTitle("url", for: .normal)
+        url.addTarget(self, action: #selector(url(sender:)), for: .touchUpInside)
+        view.addSubview(url)
+        url.snp.makeConstraints { make in
+            make.left.equalTo(stop.snp.right)
+            make.top.equalTo(mute.snp.bottom)
+        }
+        
+        // http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8
+        
+        // http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8
+        
+        player.delegate = self
+        player.play(with: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8")
+        player.backgroundColor = .black
+        view.addSubview(player)
+        player.snp.makeConstraints { make in
+            make.left.right.centerY.equalToSuperview()
+            make.height.equalTo(300)
+        }
+        
+        /**
+        let options: IJKFFOptions = IJKFFOptions.byDefault()
+                
+        // 1为开启硬件解码，用特定方法把数字编码还原成它所代表的内容或将电脉冲信号转换成它所代表的信息、数据等; 硬件解码其实就是用GPU的专门模块编码来解（建议使用硬解码）。0为软件解码，更稳定，是cpu进行解码(如果使用软解码可能导致cpu占用率高)
+        options.setPlayerOptionIntValue(1, forKey: "videotoolbox")
+        // 帧速率(fps) （可以改，确认非标准桢率会导致音画不同步，所以只能设定为15或者29.97）帧速率越大,画质越好,但太大了,有些机器版本不支持,反而有些卡。
+        options.setPlayerOptionIntValue(Int64(29.97), forKey: "r")
+        // -vol——设置音量大小，256为标准音量。（要设置成两倍音量时则输入512，依此类推
+        options.setPlayerOptionIntValue(512, forKey: "vol")
+        // 是否开启环路过滤，0开启，画面质量高，解码开销大，48关闭，画面质量略差，解码开销小
+        options.setPlayerOptionIntValue(48, forKey: "skip_loop_filter")
+        // 播放前预加载大小,默认1兆，数字越小，出画面的越快
+        options.setPlayerOptionIntValue(1024 * 8, forKey: "probesize")
+        // 播放前的探测时间
+        options.setPlayerOptionIntValue(50000, forKey: "analyzeduration")
+        // 是否开启预缓冲 0关闭，1开启，直播项目一般开启，以达到秒开效果，不过可能会出现丢帧卡顿现象
+        options.setPlayerOptionIntValue(1, forKey: "packet-buffering")
+        // 设置播放重连次数
+        options.setPlayerOptionIntValue(1, forKey: "reconnect")
+        // 跳帧处理，当CPU处理较慢时就跳帧，保证声音和画面同步
+        options.setPlayerOptionIntValue(1, forKey: "framedrop")
+        // 最大fps
+        options.setPlayerOptionIntValue(30, forKey: "max-fps")
+        // 自动转屏开关
+        //options.setPlayerOptionIntValue(0, forKey: "auto_convert")
+        
+        // 清空DNS,有时因为在APP里面要播放多种类型的视频(如:MP4,直播,直播平台保存的视频,和其他http视频), 有时会造成因为DNS的问题而报10000问题
+        //options.setPlayerOptionIntValue(1, forKey: "dns_cache_clear")
+        
+        // 静音 1开启静音， 0关闭静音
+        //options.setPlayerOptionIntValue(0, forKey: "an")
+        // 是否有视频
+        //options.setPlayerOptionIntValue(0, forKey: "vn")
+        
+        // 最大缓冲比
+        //options.setPlayerOptionIntValue(1024 * 10, forKey: "max-buffer-size")
+        // 超时时间，timeout参数只对http设置有效，若果你用rtmp设置timeout，ijkplayer内部会忽略timeout参数。rtmp的timeout参数含义和http的不一样。
+        //options.setPlayerOptionIntValue(30 * 1000 * 1000, forKey: "timeout")
+        
+        options.setPlayerOptionIntValue(0, forKey: "http-detect-range-support")
+        options.setPlayerOptionIntValue(25, forKey: "min-frames")
+        options.setPlayerOptionIntValue(1, forKey: "start-on-prepared")
+        options.setPlayerOptionIntValue(8, forKey: "skip_frame")
+         */
+    }
+    
+    @objc func shouldAutoplay(sender: UIButton) {
+        player.shouldAutoplay = !player.shouldAutoplay
+    }
+    
+    @objc func mute(sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        player.muted(sender.isSelected)
+    }
+    
+    @objc func play(sender: UIButton) {
+        player.play()
+    }
+    
+    @objc func pause(sender: UIButton) {
+        player.pause()
+    }
+    
+    @objc func stop(sender: UIButton) {
+        player.stop()
+    }
+    
+    @objc func url(sender: UIButton) {
+        player.play(with: "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8")
+    }
+    
+    deinit {
+        wy_print("WYTestLiveStreamingController release")
+    }
 
     /*
     // MARK: - Navigation
@@ -20,4 +176,42 @@ class WYTestLiveStreamingController: UIViewController {
     }
     */
 
+}
+
+extension WYTestLiveStreamingController: WYLivePlayerDelegate {
+    
+    func livePlayerDidChangeState(_ player: WYLivePlayer, _ state: WYLivePlayerState) {
+        switch state {
+        case .unknown:
+            wy_print("未知状态")
+        case .rendered:
+            wy_print("第一帧渲染完成")
+        case .ready:
+            wy_print("可以播放了")
+        case .playing:
+            wy_print("正在播放")
+            WYActivity.dismissLoading(in: player)
+        case .buffering:
+            wy_print("缓冲中")
+            WYActivity.showLoading(in: player, delay: 1)
+        case .playable:
+            wy_print("缓冲结束")
+        case .paused:
+            wy_print("播放暂停")
+        case .interrupted:
+            wy_print("播放被中断")
+        case .seekingForward:
+            wy_print("快进")
+        case .seekingBackward:
+            wy_print("快退")
+        case .stopped:
+            wy_print("停止播放")
+        case .ended:
+            wy_print("播放完毕")
+        case .userExited:
+            wy_print("用户中断播放")
+        case .error:
+            wy_print("播放出现异常")
+        }
+    }
 }
---
+++ WYBasisKit/LivePlayer/WYLivePlayer.swift
@@ -0,0 +1,283 @@
+//
+//  WYLivePlayer.swift
+//  WYBasisKit
+//
+//  Created by jacke·xu on 2022/4/21.
+//  Copyright © 2022 Jacke·xu. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+import IJKMediaFramework
+
+/// 播放器状态回调
+@objc public enum WYLivePlayerState: Int {
+    /// 未知状态
+    case unknown
+    /// 第一帧渲染完成
+    case rendered
+    /// 可以播放了
+    case ready
+    /// 正在播放
+    case playing
+    /// 缓冲中
+    case buffering
+    /// 缓冲结束
+    case playable
+    /// 播放暂停
+    case paused
+    /// 播放被中断
+    case interrupted
+    /// 快进
+    case seekingForward
+    /// 快退
+    case seekingBackward
+    /// 停止播放
+    case stopped
+    /// 播放完毕
+    case ended
+    /// 用户中断播放
+    case userExited
+    /// 播放出现异常
+    case error
+}
+
+@objc public protocol WYLivePlayerDelegate {
+    
+    /// 播放器状态回调
+    @objc optional func livePlayerDidChangeState(_ player: WYLivePlayer, _ state: WYLivePlayerState)
+}
+
+public class WYLivePlayer: UIImageView {
+    
+    /// 播放器组件
+    public var ijkPlayer: IJKFFMoviePlayerController?
+    
+    /// 当前正在播放的流地址
+    public private(set) var mediaUrl: String = ""
+    
+    /// 播放器配置选项 具体配置可参考 https://github.com/Bilibili/ijkplayer/blob/master/ijkmedia/ijkplayer/ff_ffplay_options.h
+    public var options: IJKFFOptions?
+    
+    /// 播放器状态回调代理
+    public weak var delegate: WYLivePlayerDelegate?
+    
+    /// 循环播放的次数(点播流有效)
+    public var looping: Int64 = Int64(INT_MAX)
+    
+    /// 播放失败后重试次数，默认2次
+    public var failReplay: Int = 2
+    
+    /// 是否需要自动播放
+    public var shouldAutoplay: Bool = true
+    
+    /// 播放画面显示模式
+    public var scalingMode: IJKMPMovieScalingMode = .aspectFill
+    
+    /// 是否需要静音
+    public func muted(_ mute: Bool) {
+        ijkPlayer?.playbackVolume = mute ? 0 : 1
+    }
+    
+    /// 继续播放(仅适用于暂停后恢复播放)
+    public func play() {
+        ijkPlayer?.play()
+    }
+    
+    /// 暂停播放
+    public func pause() {
+        ijkPlayer?.pause()
+    }
+    
+    /**
+     * 停止播放(无法再次恢复播放)
+     * @param keepLast 是否要保留最后一帧图像
+     */
+    public func stop(_ keepLast: Bool = true) {
+        
+        guard let player = ijkPlayer else {
+            return
+        }
+        
+        if keepLast {
+            image = player.thumbnailImageAtCurrentTime()
+        }
+        options = nil
+        releasePlayer()
+    }
+    
+    /// 释放播放器组件
+    public func releasePlayer() {
+        
+        guard let player = ijkPlayer else {
+            return
+        }
+        player.stop()
+        
+        NotificationCenter.default.removeObserver(self, name: .IJKMPMoviePlayerPlaybackDidFinish, object: ijkPlayer)
+        NotificationCenter.default.removeObserver(self, name: .IJKMPMoviePlayerPlaybackStateDidChange, object: ijkPlayer)
+        NotificationCenter.default.removeObserver(self, name: .IJKMPMoviePlayerLoadStateDidChange, object: ijkPlayer)
+        NotificationCenter.default.removeObserver(self, name: .IJKMPMoviePlayerFirstVideoFrameRendered, object: ijkPlayer)
+        
+        ijkPlayer?.shutdown()
+        ijkPlayer?.view.removeFromSuperview()
+        ijkPlayer = nil
+    }
+    
+    /**
+     * 开始播放
+     * @param url 要播放的流地址
+     * @param background 视屏背景图(支持UIImage、URL、String)
+     */
+    public func play(with url: String, background: Any? = nil, placeholderImage: UIImage? = nil) {
+        
+        image = nil
+        isUserInteractionEnabled = true
+        
+        if mediaUrl != url {
+            failReplayNumber = 0
+        }
+        
+        releasePlayer()
+        createPlayer(with: url)
+        ijkPlayer?.prepareToPlay()
+        
+        mediaUrl = url
+        
+        if let imageUrl: URL = background as? URL {
+            kf.setImage(with: URL(string: (imageUrl.absoluteString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")), placeholder: placeholderImage)
+        }
+        
+        if let imageString: String = background as? String {
+            kf.setImage(with: URL(string: (imageString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")), placeholder: placeholderImage)
+        }
+        
+        if let backgroundImage: UIImage = background as? UIImage {
+            image = backgroundImage
+        }
+    }
+    
+    /// 当前已重试失败次数
+    private var failReplayNumber: Int = 0
+    
+    /// 创建播放器组件
+    private func createPlayer(with url: String) {
+        
+        if options == nil {
+            options = IJKFFOptions.byDefault()
+            options?.setPlayerOptionIntValue(1, forKey: "videotoolbox")
+            options?.setPlayerOptionIntValue(Int64(29.97), forKey: "r")
+            options?.setPlayerOptionIntValue(512, forKey: "vol")
+            options?.setPlayerOptionIntValue(48, forKey: "skip_loop_filter")
+            options?.setPlayerOptionIntValue(1024 * 5, forKey: "probesize")
+            options?.setPlayerOptionIntValue(1, forKey: "packet-buffering")
+            options?.setPlayerOptionIntValue(1, forKey: "reconnect")
+            options?.setPlayerOptionIntValue(looping, forKey: "loop")
+            options?.setPlayerOptionIntValue(1, forKey: "framedrop")
+            options?.setPlayerOptionIntValue(30, forKey: "max-fps")
+            options?.setPlayerOptionIntValue(0, forKey: "http-detect-range-support")
+            options?.setPlayerOptionIntValue(25, forKey: "min-frames")
+            options?.setPlayerOptionIntValue(1, forKey: "start-on-prepared")
+            options?.setPlayerOptionIntValue(8, forKey: "skip_frame")
+        }
+        ijkPlayer = IJKFFMoviePlayerController(contentURL: URL(string: url), with: options)
+        ijkPlayer?.shouldAutoplay = true
+        ijkPlayer?.scalingMode = scalingMode
+        addSubview((ijkPlayer?.view)!)
+        ijkPlayer?.view.snp.makeConstraints({ make in
+            make.edges.equalToSuperview()
+        })
+        IJKFFMoviePlayerController.setLogLevel(k_IJK_LOG_ERROR)
+        
+        // 播流完成回调
+        NotificationCenter.default.addObserver(self, selector: #selector(ijkPlayerDidFinished(notification:)), name: .IJKMPMoviePlayerPlaybackDidFinish, object: ijkPlayer)
+        
+        // 用户操作行为回调
+        NotificationCenter.default.addObserver(self, selector: #selector(ijkPlayerPlayStateDidChange(notification:)), name: .IJKMPMoviePlayerPlaybackStateDidChange, object: ijkPlayer)
+        
+        // 直播加载状态回调
+        NotificationCenter.default.addObserver(self, selector: #selector(ijkPlayerLoadStateDidChange(notification:)), name: .IJKMPMoviePlayerLoadStateDidChange, object: ijkPlayer)
+        
+        // 渲染回调
+        NotificationCenter.default.addObserver(self, selector: #selector(ijkPlayerLoadStateDidRendered(notification:)), name: .IJKMPMoviePlayerFirstVideoFrameRendered, object: ijkPlayer)
+    }
+    
+    @objc private func ijkPlayerDidFinished(notification: Notification) {
+        
+        if let reason: IJKMPMovieFinishReason = notification.userInfo?[IJKMPMoviePlayerPlaybackDidFinishReasonUserInfoKey] as? IJKMPMovieFinishReason {
+            switch reason {
+            case .playbackEnded:
+                delegate?.livePlayerDidChangeState?(self, .ended)
+            case .playbackError:
+                delegate?.livePlayerDidChangeState?(self, .error)
+                
+                if failReplayNumber < failReplay {
+                    failReplayNumber += 1
+                    play(with: mediaUrl)
+                }else {
+                    releasePlayer()
+                }
+            case .userExited:
+                delegate?.livePlayerDidChangeState?(self, .userExited)
+            default:
+                break
+            }
+        }
+    }
+    
+    @objc private func ijkPlayerPlayStateDidChange(notification: Notification) {
+        
+        guard let player = ijkPlayer else { return }
+        
+        switch player.playbackState {
+        case .playing:
+            delegate?.livePlayerDidChangeState?(self, .playing)
+        case .paused:
+            delegate?.livePlayerDidChangeState?(self, .paused)
+        case .interrupted:
+            delegate?.livePlayerDidChangeState?(self, .interrupted)
+        case .seekingForward:
+            delegate?.livePlayerDidChangeState?(self, .seekingForward)
+        case .seekingBackward:
+            delegate?.livePlayerDidChangeState?(self, .seekingBackward)
+        case .stopped:
+            delegate?.livePlayerDidChangeState?(self, .stopped)
+        default:
+            break
+        }
+    }
+    
+    @objc private func ijkPlayerLoadStateDidChange(notification: Notification) {
+        
+        guard let player = ijkPlayer else { return }
+        
+        switch player.loadState {
+        case .playable:
+            delegate?.livePlayerDidChangeState?(self, .playable)
+        case .playthroughOK:
+            delegate?.livePlayerDidChangeState?(self, .ready)
+        case .stalled:
+            delegate?.livePlayerDidChangeState?(self, .buffering)
+        default:
+            delegate?.livePlayerDidChangeState?(self, .unknown)
+        }
+    }
+    
+    @objc private func ijkPlayerLoadStateDidRendered(notification: Notification) {
+        delegate?.livePlayerDidChangeState?(self, .rendered)
+    }
+    
+    deinit {
+        releasePlayer()
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
